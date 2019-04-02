e3e54953240415538dae08ad301a1f40edb29c5c
Brian Guerrero <13bguerrero@gmail.com>
2019-04-02 14:08:32 -0500

    Live pins demo.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: []
+    }
+    this.initAutocomplete = this.initAutocomplete.bind(this);
+    this.addMapMarker = this.addMapMarker.bind(this);
+  }
+
+  initAutocomplete(mapProps) {
+    const { google } = mapProps;
+    let autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete')
+    )
+    autocomplete.setFields(["geometry"]);
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        this.addMapMarker(place.geometry.location);
+      }
+    });
+    this.forceUpdate();
+  }
+
+  addMapMarker(marker) {
+    this.setState({
+      markers: [{ lat: marker.lat(), lng: marker.lng() }]
+    })
+  }
+
   render() {
+    let markers = Array.from(this.state.markers);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" id="autocomplete" />
+        <Map
+          google={this.props.google}
+          initialCenter={{
+            lat: 41.503152,
+            lng: -90.550617
+          }}
+          onClick={this.props.google}
+          zoom={14}
+          onReady={this.initAutocomplete}
+        >
+          {markers.map((marker, index) => (
+            <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+          ))}
+        </Map>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'ADD API KEY HERE'
+})(App);
